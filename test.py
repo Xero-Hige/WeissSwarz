a4e4ad06ee2239951a9d22dbc0270d59064d996d
Xero-Hige <gaston.martinez.90@gmail.com>
2015-05-23 01:47:31 -0300

    Reformateo el codigo

--- test.py
+++ test.py
@@ -14,14 +14,15 @@ class GameBoard(object):
 
         self.waiting_room = []
 
-        self.front_stage = [None,None,None]
-        self.back_stage = [None,None]
+        self.front_stage = [None, None, None]
+        self.back_stage = [None, None]
+
 
 class Deck(object):
     """Simulates a deck"""
 
-    def __init__(self,deckname):
-        #TODO: open file
+    def __init__(self, deckname):
+        # TODO: open file
 
         self.cards = []
 
@@ -29,14 +30,16 @@ class Deck(object):
         """ Draws a card from the deck """
         return self.cards.pop()
 
+
 class Card(object):
     """ Simulates a card  """
     pass
 
+
 class CharacterCard(Card):
     """Simulates a character card"""
 
-    def __init__(self,attack):
+    def __init__(self, attack):
         """ Creates a card with the info  """
         self.atk = attack
 
