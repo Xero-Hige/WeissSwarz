9e50d43fc4099c1a1be2f74b1943304dba7af4d6
Xero-Hige <gaston.martinez.90@gmail.com>
2015-05-23 01:46:59 -0300

    Agrego esqueleto de Gameboard , deck y card

---
+++ test.py
@@ -0,0 +1,45 @@
+__author__ = 'hige'
+
+
+class GameBoard(object):
+    """Simulates the gameboard"""
+
+    def __init__(self):
+        """Creates an empty gameboard"""
+        self.clock = []
+        self.stock = []
+        self.level = []
+
+        self.climax = None
+
+        self.waiting_room = []
+
+        self.front_stage = [None,None,None]
+        self.back_stage = [None,None]
+
+class Deck(object):
+    """Simulates a deck"""
+
+    def __init__(self,deckname):
+        #TODO: open file
+
+        self.cards = []
+
+    def draw_card(self):
+        """ Draws a card from the deck """
+        return self.cards.pop()
+
+class Card(object):
+    """ Simulates a card  """
+    pass
+
+class CharacterCard(Card):
+    """Simulates a character card"""
+
+    def __init__(self,attack):
+        """ Creates a card with the info  """
+        self.atk = attack
+
+
+board = GameBoard()
+deck = Deck("swartz")
