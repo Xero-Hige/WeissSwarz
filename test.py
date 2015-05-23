3ac96a0920622f3ec18e073cfd6d9ee3df138909
Xero-Hige <gaston.martinez.90@gmail.com>
2015-05-23 03:31:08 -0300

    Agrego los atributos de todas las cartas y de las cartas de personajes

--- test.py
+++ test.py
@@ -37,19 +37,27 @@ class Deck(object):
 
 class Card(object):
     """ Simulates a card  """
-    pass
+
+    def __init__(self, name, color, trigger_icon, hability):
+        self.name = name
+        self.color = color
+        self.trigger_icon = trigger_icon
+        self.hability = hability
 
 
 class CharacterCard(Card):
     """Simulates a character card"""
 
-    def __init__(self, attack):
+    def __init__(self, name, color, trigger_icon, hability, level, cost, power, soul_points, trait):
         """ Creates a card with the info  """
-        self.atk = attack
+        super(self.__class__, self).__init__(name, color, trigger_icon, hability)
+        self.level = level
+        self.cost = cost
+        self.power = power
+        self.soul_points = soul_points
+        self.trait = trait
 
 
 board = GameBoard()
 deck = Deck("swartz")
 card = deck.draw_card()
-
-print card.atk
