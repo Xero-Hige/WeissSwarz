db8f19ee0f6d7bea543641b8310f5b3abb2bb334
Xero-Hige <gaston.martinez.90@gmail.com>
2015-05-23 21:54:04 -0300

    Arreglo algunas cosas que estaban mal, y agrego unos getters

--- cards.py
+++ cards.py
@@ -11,35 +11,47 @@ class Card(object):
         self.hability = hability
 
     def __str__(self):
-        return self.name
+        return self.name + "(" + self.color + ")"
 
     def __repr__(self):
         return self.name + "(" + self.color + ")"
 
     def get_color(self):
-        return self.name + "(" + self.color + ")"
+        return self.color
 
     def get_trigger_icon(self):
         return self.trigger_icon
+
+    def get_name(self):
+        return self.name
 
 
 class CharacterCard(Card):
     """Simulates a character card"""
 
-    def __init__(self, name, color, trigger_icon, hability, level, cost, power, soul_points, trait):
+    def __init__(self, name, color, trigger_icon, hability, level, cost, power, soul_points, traits):
         """ Creates a card with the info  """
         super(self.__class__, self).__init__(name, color, trigger_icon, hability)
         self.level = level
         self.cost = cost
         self.power = power
         self.soul_points = soul_points
-        self.trait = trait
+        self.traits = traits
 
     def get_level(self):
         return self.level
 
     def get_cost(self):
         return self.cost
+
+    def get_power(self):
+        return self.power
+
+    def get_soul_points(self):
+        return self.soul_points
+
+    def get_traits(self):
+        return self.traits
 
 
 class ClimaxCard(Card):
